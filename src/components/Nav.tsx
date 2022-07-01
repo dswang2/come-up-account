6bb9639e694c7e30bc6e01446a03b3f32cbe47c7
wangdisheng <wangdishengyx@163.com>
2022-07-01 16:30:22 +0800

    从0开发React项目-8-首页导航栏布局-svg作为图片使用

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import money from "icons/money.svg"
+
 const NavWrapper = styled.nav`
   line-height: 24px; // 不特别设置的话，line-height等于font-size+行距，是文字实际占用尺寸，一般不直接改变标签大小，而是改变line-height
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25); // 给某一快区域加上阴影效果
@@ -20,6 +22,7 @@ const NavWrapper = styled.nav`
 const Nav = () => {
     return (<NavWrapper>
         <ul>
+            <img src={money} alt=""/>
             <li><Link to="/tags">标签页</Link></li>
             <li><Link to="/money">记账页</Link></li>
             <li><Link to="/statistics">统计页</Link></li>
