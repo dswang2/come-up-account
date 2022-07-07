07a89f28a72762523959646fcf467d02dc86ec17
wangdisheng <wangdishengyx@163.com>
2022-07-08 00:38:54 +0800

    从0开发React项目-14-标签组件的逻辑功能,TagsSection

--- src/views/money/TagsSection.tsx
+++ src/views/money/TagsSection.tsx
@@ -1,16 +1,42 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
     children?: React.ReactNode;
 }
-const TagsSection: React.FunctionComponent<Props> = (props:Props) => {
+const TagsSection: React.FunctionComponent<Props> = () => {
+    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+    const onAddTag = () => {
+        const tagName = window.prompt("新标签名称为");
+        if (tagName !== null) {
+            setTags([...tags, tagName]);
+        }
+    }
+    const onToggleTag = (tag: string) => {
+        const index = selectedTags.indexOf(tag);
+        if (index >= 0) {
+            setSelectedTags(selectedTags.filter((t) => t !== tag));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
+    }
+    const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
     return (
-        <_TagsSection>{props.children}</_TagsSection>
+        <Wrapper>
+            <ol>
+                {tags.map((tag) => {
+                    return <li key={tag} onClick={() => {
+                        onToggleTag(tag)
+                    }} className={getClass(tag)}>{tag}</li>
+                })}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
     );
 }
 
-const _TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #FFFFFF;
   padding: 12px 16px;
   flex-grow: 1;
@@ -30,6 +56,10 @@ const _TagsSection = styled.section`
       font-size: 14px;
       line-height: 16px;
       margin: 8px 12px;
+
+      &.selected {
+        background: #f60;
+      }
     }
   }
 
