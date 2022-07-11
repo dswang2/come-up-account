082679c03e0b2ae6e51fb24adf38770d8f50ed03
wangdisheng <wangdishengyx@163.com>
2022-07-12 04:52:18 +0800

    从0开发React项目-14-标签组件的逻辑功能,Category1

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -10,12 +10,7 @@ const Money = () => {
         <MyLayout>
             <TagsSection/>
             <NotesSection/>
-            <CategorySection>
-                <ul>
-                    <li className={"selected"}>支出</li>
-                    <li>收入</li>
-                </ul>
-            </CategorySection>
+            <CategorySection/>
             <NumberPadSection>
                 <div className={"output"}>100</div>
                 <div className={"pad clearfix"}>
--- src/views/money/CategorySection.tsx
+++ src/views/money/CategorySection.tsx
@@ -1,27 +1,50 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const CategorySection = styled.section`
-    >ul{
-      display: flex;
-      background: #c4c4c4;
-      font-size: 24px;
-      >li{
-        width: 50%;
-        text-align: center;
-        padding: 16px 0;
-        position: relative;
-        &.selected::after{
-          content: '';
-          display: block;
-          height: 3px;
-          background: #333;
-          position: absolute;
-          bottom: 0px;
-          width: 100%;
-          left: 0;
-        }
+type Props = {
+    children?: React.ReactNode;
+}
+const CategorySection: React.FunctionComponent<Props> = () => {
+    const [category, setCategory] = useState('-');
+    return (<Wrapper>
+        <ul>
+            <li className={category == '-' ? "selected" : ""}
+                onClick={() => {
+                    setCategory('-');
+                }}>支出
+            </li>
+            <li className={category == '+' ? "selected" : ""}
+                onClick={() => {
+                    setCategory('+');
+                }}>收入
+            </li>
+        </ul>
+    </Wrapper>);
+}
+const Wrapper = styled.section`
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        display: block;
+        height: 3px;
+        background: #333;
+        position: absolute;
+        bottom: 0px;
+        width: 100%;
+        left: 0;
       }
     }
+  }
 `;
 
-export {CategorySection}
+export { CategorySection }
