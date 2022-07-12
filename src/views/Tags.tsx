a4f6a73ad126e6fadc4630c1ac7508a5adde0ea0
wangdisheng <wangdishengyx@163.com>
2022-07-13 04:58:44 +0800

    从0开发React项目-15-标签页开发,标签页UI(标签过多的显示问题未解)

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,8 +1,66 @@
+import styled from "styled-components";
 import { Layout } from "../components/Layout";
+import { useTags } from "../hooks/useTags";
+import { Icon } from "../components/Icon";
 
 const Tags = () => {
+    const { tags, setTags } = useTags();
     return (
-        <Layout><h2>标签页面</h2></Layout>
+        <Layout>
+            <Wrapper>
+                <ol>
+                    {tags.map((tag) => {
+                        return (<li>
+                            {tag}
+                            <Icon name={"right"}/>
+                        </li>);
+                    })}
+                </ol>
+                <button>新建标签</button>
+            </Wrapper>
+        </Layout>
     );
 }
-export {Tags};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  > ol {
+    width: 100%;
+    font-size: 16px;
+    background: white;
+    flex-grow: 1; // 膨胀指数
+    overflow: auto;
+    > li {
+      //background: white;
+      border-bottom: 1px solid #BCBBC1;
+      //box-shadow: inset 0px -0.5px 0px #BCBBC1;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 12px 16px 16px 0px;
+      margin-left: 16px;
+      line-height: 16px;
+    }
+  }
+
+  > button {
+    margin-top: 52px;
+    margin-left: auto;
+    margin-right: auto;
+    border: none;
+    background: #767676;
+    border-radius: 4px;
+    font-size: 18px;
+    line-height: 20px;
+    padding: 10px 16px;
+    color: white;
+    flex-grow: 0; // 膨胀指数
+    overflow: auto;
+  }
+
+`
+
+export { Tags };
