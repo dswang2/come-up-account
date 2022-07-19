1fb54fbf67b253b88f1e4d851b32374af6374b68
wangdisheng <wangdishengyx@163.com>
2022-07-19 16:01:46 +0800

    从0开发React项目-16-数据持久化，createId优化

--- src/lib/createId.tsx
+++ src/lib/createId.tsx
@@ -1,6 +1,9 @@
-let id = 0;
+import { useTags } from "../hooks/useTags";
+
 const createId = ():number => {
-    id = id + 1;
+    const tags =JSON.parse(window.localStorage.getItem("tags")||'[]');
+    const preId = tags.length > 0 ? tags[tags.length-1].id : 0;
+    const id = preId + 1;
     return id;
 }
 export { createId };
