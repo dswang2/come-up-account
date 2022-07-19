66051abee68f3ecd4ac7ff296f4f65bc99f0611f
wangdisheng <wangdishengyx@163.com>
2022-07-19 15:34:15 +0800

    从0开发React项目-16-数据持久化，使用useEffect

--- src/hooks/useTags.tsx
+++ src/hooks/useTags.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createId } from "../lib/createId";
 import { TagEntity } from "../types/Tag";
 
-const defaultTags = [
-    { id: createId(), name: '衣' },
-    { id: createId(), name: '食' },
-    { id: createId(), name: '住' },
-    { id: createId(), name: '行' }
-];
 const useTags = () => {
-    const [tags, setTags] = useState<TagEntity[]>(defaultTags);
+    const [tags, setTags] = useState<TagEntity[]>([]);
+    useEffect(() => {
+        setTags(JSON.parse(window.localStorage.getItem("tags")||'[]'));
+    },[]); // 依赖数据为空数组时，表示初始化
+    useEffect(() => {
+        window.localStorage.setItem("tags", JSON.stringify(tags));
+    },[tags]); // tags引用更新时触发，这也就要求，更新tags，不能只更新值，而是要更新tags的地址
     const updateTag = (id: number, name: string) => {
         setTags(tags.map((t) => {
             return t.id === id ? { id, name } : t;
