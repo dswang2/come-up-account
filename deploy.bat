88f48f54ac9a4c5f72190049d35f90d55828e5e8
wangdisheng <wangdishengyx@163.com>
2022-07-23 13:20:10 +0800

    从0开发React项目-18-部署与优化，部署脚本

---
+++ deploy.bat
@@ -0,0 +1,1 @@
+yarn build && cd build && git init && git add . && git commit -m %1 && git remote add origin git@github.com:dswang2/come-up-account-website.git && git branch -M main && git push -u origin main -f && cd ..
